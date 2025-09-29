a8917ddb07cf239ee10a6c36a3f3fd29ad771c0a
KojiTakahara <k.takahara@rakumo.com>
2025-09-29 12:37:34 +0900

    refactor: Apply ESLint configuration feedback for #39

--- othello/.eslintrc.cjs
+++ othello/.eslintrc.cjs
@@ -4,19 +4,27 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.app.json'], // tsconfigファイルのパスを指定
+    tsconfigRootDir: __dirname, // プロジェクトのルートディレクトリを指定
+  },
+  settings: { react: { version: 'detect' } }, // '18.2'から'detect'に変更
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^' }], // 未使用変数の警告を強化
   },
 }
